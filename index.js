640021cb282d83a832a9f90fa07350ba537fa039
Franklin <jesusfranklinimmanuel2000@gmail.com>
2024-11-25 11:02:33 +0530

    Modified

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const path = require("path");
 const filename = path.dirname(__filename);
 const dirname = path.basename(__dirname);
 const extension = path.extname(__filename);
-const resolved = path.resolve()
 
 console.log("filename", filename);  
 console.log("dirname", dirname);
