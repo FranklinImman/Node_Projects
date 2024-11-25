f13b8139152e0d4a977f156f316bad16491da6ad
Franklin Immanuel N <162884510+FranklinImman@users.noreply.github.com>
2024-11-25 11:09:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 console.log("filename",__filename);
 console.log("dirname",__dirname);
 const path = require("path");
+const fs = require("fs");
+
+fs.readFile('example.txt','utf8',(err,data)=>{
+  if(err){
+    console.error("Error reading the file: ",err);
+  }
+  console.log("Data: ",data);
+});
 
 const filename = path.dirname(__filename);
 const dirname = path.basename(__dirname);
